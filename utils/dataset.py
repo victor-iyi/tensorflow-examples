ab4bf48167042be4e4c16096bf386caf253b78ce
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-16 16:05:15 +0100

    Dataset utility file.

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,7 +19,15 @@ def fake_data(n, size=28, channels=1):
         channels {int} -- How many color channels. (default: {1})
 
     Returns:
-        [list of ndarray] -- image and it's corresponding label.
+        [{list}, {ndarray}] -- image and it's corresponding label.
+
+    Example:
+        >>> num_examples = 128
+        >>> X, y = fake_data(num_examples, size=32, channels=3)
+        >>> X.shape
+        (128, 32, 32, 3)	
+        >>> y.shape
+        (128,)
     """
 
     data = np.ndarray(shape=[n, size, size, channels],
@@ -30,6 +40,50 @@ def fake_data(n, size=28, channels=1):
         labels[i] = label
 
     return data, labels
+
+
+def gen_data(file, max=50):
+    """Generate a Python code dataset.
+    Using the builtin standard libraries as a reference.
+    It works by combining {max} number of the reference files.
+
+    Arguments:
+        file {str} -- Name of the file to be written into.
+
+    Keyword Arguments:
+        max {int} -- Maximum number of files to join. (default: {50})
+
+    Example:
+        >>> data_path = 'datasets/python_code.py'
+        >>> gen_data(data_path, max=20)
+        >>> os.path.isfile(data_path)
+        True
+    """
+
+    PYTHON_HOME = os.path.join('/Library/Frameworks/Python.framework',
+                               'Versions/3.6/lib/python3.6/')
+    if os.path.isfile(file):
+        import shutil
+        shutil.rmtree(os.path.dirname(file))
+
+    if not os.path.isdir(os.path.dirname(file)):
+        os.makedirs(os.path.dirname(file))
+
+    handle = open(file, 'a', encoding='utf-8')
+
+    counter = 0
+    for (root, _, files) in os.walk(PYTHON_HOME):
+        files = [os.path.join(root, f) for f in files if f.endswith('.py')]
+        for _file in files:
+            try:
+                code = open(_file, 'r', encoding='utf-8').read()
+                handle.write(str(code) + '\n')
+            except Exception as e:
+                print('Exception: {e}'.format(e))
+
+        counter += 1
+        if counter > max:
+            break
 
 
 if __name__ == '__main__':
