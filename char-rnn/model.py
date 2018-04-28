c8c4f9c034fa0dab1b3ff9aa995479abd6453cf8
Victor I. Afolabi <javafolabi@gmail.com>
2018-04-28 21:56:34 +0100

    Added more command line arguments.

--- char-rnn/model.py
+++ char-rnn/model.py
@@ -13,13 +13,13 @@ class Model:
             self.args.batch_size = 1
             self.args.seq_length = 1
 
-        if self.args.model == 'rnn':
+        if self.args.model.lower() == 'rnn':
             cell_fn = rnn.BasicRNNCell
-        elif self.args.model == 'lstm':
+        elif self.args.model.lower() == 'lstm':
             cell_fn = rnn.BasicLSTMCell
-        elif self.args.model == 'gru':
+        elif self.args.model.lower() == 'gru':
             cell_fn = rnn.GRUCell
-        elif self.args.model == 'nas':
+        elif self.args.model.lower() == 'nas':
             cell_fn = rnn.NASCell
         else:
             raise ValueError("Model type not supported.")
@@ -27,4 +27,4 @@ class Model:
         cells = []
         for _ in range(self.args.num_layers):
             cell = cell_fn(self.args.rnn_size)
-            
+
--- char-rnn/train.py
+++ char-rnn/train.py
@@ -2,15 +2,58 @@ import argparse
 
 
 def main():
+    # Argument parser.
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    # Command line arguements.
     parser.add_argument('--data_dir', type=str, default='data/pycode',
                         help='Data directory containing input.txt')
     parser.add_argument('--save_dir', type=str, default='save',
                         help='Directory where checkpoints are stored.')
     parser.add_argument('--logdir', type=str, default='logs',
                         help='Tensorboard log directory.')
+    parser.add_argument('--rnn_size', type=int, default=128,
+                        help='Size of RNN hidden cell state.')
+    parser.add_argument('--num_layers', type=int, default=2,
+                        help='Number of hidden layers in the network.')
+    parser.add_argument('--model', type=str, default='lstm',
+                        help='Recurrent architecture: RNN, LSTM, GRU or NAS')
+    parser.add_argument('==batch_size', type=int,
+                        default=50, help='Mini batch size.')
+    parser.add_argument('--seq_length', type=int, default=50,
+                        help='Recurrent sequence length.')
+    parser.add_argument('--num_epochs', type=int, default=10,
+                        help='Number of training epochs.')
+    parser.add_argument('--save_every', type=int, default=500,
+                        help='Save frequency.')
+    parser.add_argument('--grad_clip', type=float, default=5.,
+                        help='Clip gradient at this value.')
+    parser.add_argument('--learning_rate', type=float, default=1e-2,
+                        help='Learning rate.')
+    parser.add_argument('--decay_rate', type=float, default=0.97,
+                        help='Decay rate for RMSProp.')
+    parser.add_argument('--input_keep_prob', type=float, default=1.0,
+                        help='Probability of keeping weights in the hidden layers.')
+    parser.add_argument('--output_keep_prob', type=float, default=1.0,
+                        help='Probability of keeping weights in the output layer.')
+    parser.add_argument('--init_from', type=str, default=None,
+                        help="""continue training from saved model at this path. Path must contain files saved by previous training process:
+                            'config.pkl'        : configuration;
+                            'chars_vocab.pkl'   : vocabulary definitions;
+                            'checkpoint'        : paths to model file(s) (created by tf).
+                                                  Note: this file contains absolute paths, be careful when moving files around;
+                            'model.ckpt-*'      : file(s) with model definition (created by tf)""")
+
+    # Parse the arguments.
+    args = parser.parse_args()
+
+    # Call the train function to begin training.
+    train(args)
+
+
+def train(args):
+    pass
 
 
 if __name__ == '__main__':
