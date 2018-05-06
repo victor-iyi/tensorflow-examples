fe673eb3dbba2e0c8cd09b3a3eca13d5ef37df89
Victor I. Afolabi <javafolabi@gmail.com>
2018-05-06 20:45:09 +0100

    Calculating the percentage of each class labels.

--- eager-execution/iris-classification.py
+++ eager-execution/iris-classification.py
@@ -13,14 +13,67 @@
     MIT License
     Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
 """
-
-from sklearn import datasets, preprocessing, model_selection
+import numpy as np
 
 import tensorflow as tf
 from tensorflow.contrib.eager.python import tfe
 
 # Turn on eager execution.
 tf.enable_eager_execution()
+
+
+def _parse_line(line):
+    """Perform pre-processing on each row in the CSV file.
+
+    Args:
+        line (): Each row of the CSV file.
+
+    Returns:
+        (features, labels) - Parsed features and labels.
+    """
+    row = tf.decode_csv(line, record_defaults=[[0.], [0.], [0.], [0.], [0]])
+    # Split line into features and labels.
+    features = tf.reshape(row[:-1], shape=(4,))
+    labels = tf.reshape(row[-1], shape=())
+    # Return parsed features & labels.
+    return features, labels
+
+
+def load_data():
+    """Load training and testing dataset as a tf.data.Dataset object.
+
+    Returns:
+        (tf.data.Dataset, tf.data.Dataset) - train, test
+    """
+    # Iris training and testing dataset URL. May change in the future.
+    TRAIN_URL = "http://download.tensorflow.org/data/iris_training.csv"
+    TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
+
+    # Download dataset if they exist. Otherwise load from disk.
+    train_path = tf.keras.utils.get_file(fname=TRAIN_URL.split('/')[-1],
+                                         origin=TRAIN_URL)
+    test_path = tf.keras.utils.get_file(fname=TEST_URL.split('/')[-1],
+                                        origin=TEST_URL)
+
+    # Train dataset.
+    train = tf.data.TextLineDataset(train_path)
+    train = train.skip(count=1)
+    train = train.map(_parse_line)
+    train = train.shuffle(buffer_size=1000)
+    train = train.batch(batch_size=32)
+
+    # Test dataset.
+    test = tf.data.TextLineDataset(test_path)
+    test = test.skip(count=1)
+    test = test.map(_parse_line)
+
+    # Return train & test as a tf.data.Dataset object.
+    return train, test
+
+
+"""
+# Sci-kit learn utility functions.
+from sklearn import datasets, preprocessing, model_selection
 
 # Load the Iris dataset.
 data = datasets.load_iris()
@@ -42,27 +95,24 @@ labels = labels.fit_transform(data['target'].reshape(-1, 1))
 # Split into training and testing sets.
 X_train, X_test, y_train, y_test = model_selection.train_test_split(features, labels,
                                                                     test_size=0.25)
-
-train_data = tf.data.Dataset.from_tensor_slices((X_train, X_test))
-test_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
-
 print(55 * '-')
 print('X_train.shape = {}\tX_test.shape:{}'.format(X_train.shape, X_test.shape))
 print('y_train.shape = {}\ty_test.shape:{}'.format(y_train.shape, y_test.shape))
 print(55 * '-', '\n')
 
-# Model variables
-W = tfe.Variable(0.3, name='W')
-b = tfe.Variable(0., name='b')
+# Training set.
+train_data = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+train_data = train_data.batch(batch_size=32)
 
+# Testing set.
+test_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
 
-def dense(X, W, b):
-    return tf.matmul(X, W) + b
+"""
+# Label names
+TARGET_NAMES = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
 
-
-hidden = tf.layers.Dense(units=10, activation=tf.nn.relu, name='hidden_layer')
-dropout = tf.layers.Dropout(name='dropout')
-output = tf.layers.Dense(units=3, activation=None, name='output')
+# Training and testing set.
+train_data, test_data = load_data()
 
 
 class Network(tf.keras.Model):
@@ -82,21 +132,22 @@ class Network(tf.keras.Model):
     def call(self, x, **kwargs):
         return self.output_layer(self.hidden_layer(x))
 
+    # Abstract methods.
+    def _set_inputs(self, inputs, training=None):
+        pass
+
+    def add_variable(self, name, shape, dtype=None, initializer=None,
+                     regularizer=None, trainable=True, constraint=None, **kwargs):
+        pass
+
+    def add_loss(self, *args, **kwargs):
+        pass
+
+    def save(self, filepath, overwrite=True, include_optimizer=True):
+        pass
+
 
 net = Network()
-
-# Get the variable counts before any op.
-var_len = len(net.variables)
-print('var_len = {}'.format(var_len))
-
-# Make predictions
-pred_values = tf.random_normal(shape=[5, 4])
-pred = net(pred_values)
-print('pred = {}'.format(pred))
-
-# Get the variable counts after making predictions.
-var_len = len(net.variables)
-print('var_len = {}'.format(var_len))
 
 
 def loss(model: Network, inputs: any, labels: any):
@@ -112,3 +163,14 @@ optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
 def train_step(loss: loss, model: Network, optimizer: tf.train.Optimizer, x, y):
     optimizer.minimize(loss=lambda: loss(model, x, y),
                        global_step=tf.train.get_or_create_global_step())
+
+
+# Loop through each batch in the dataset.
+for b, (X_batch, y_batch) in enumerate(tfe.Iterator(train_data)):
+    y_batch = y_batch.numpy()  # Convert to numpy array.
+    print('\nBatch {}'.format(b))
+    # Loop through each class.
+    for i, class_names in TARGET_NAMES.items():
+        indices = np.where(y_batch == i)[0]  # Get the list of
+        per = len(y_batch[indices]) / len(y_batch)
+        print('Percentage of class {!r}: {:>.2%}'.format(class_names.title(), per))
