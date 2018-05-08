ace04d6c7b7c116a4dc6c60d13239c4da4d992a7
Victor I. Afolabi <javafolabi@gmail.com>
2018-05-08 16:49:14 +0100

    Dataset example

---
+++ tensorflow-org/get_started/dataset-example.py
@@ -0,0 +1,16 @@
+"""Proper way to feed data into TensorFlow models.
+
+  @author 
+    Victor I. Afolabi
+    Artificial Intelligence & Software Engineer.
+    Email: javafolabi@gmail.com
+    GitHub: https://github.com/victor-iyiola
+  
+  @project
+    File: dataset-example.py
+    Created on 08 May, 2018 @ 04:46 AM.
+    
+  @license
+    MIT License
+    Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
+"""
