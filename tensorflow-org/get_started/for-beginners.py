88d62a6569bc48120112c11f6730cec41e325e2f
Victor I. Afolabi <javafolabi@gmail.com>
2018-05-11 19:46:36 +0100

    Documentation & annotations.

--- tensorflow-org/get_started/for-beginners.py
+++ tensorflow-org/get_started/for-beginners.py
@@ -35,7 +35,16 @@ batch_size = 32
 learning_rate = 1e-2
 
 
-def one_hot(values: np.array, dtype=np.int32):
+def one_hot(values: np.array, dtype: np.dtype = np.int32) -> np.array:
+    """Returns the one-hot encoding of `values` with data type `dtype`.
+
+    Args:
+        values (np.array): Array to be encoded.
+        dtype (np.dtype): Data type of the encoded array.
+
+    Returns:
+        hot (np.array): One-hot encoding of `values`.
+    """
     u, idx = np.unique(values, return_inverse=True)
     shape = (values.shape[0], u.shape[0])
     hot = np.zeros(shape=shape, dtype=dtype)
@@ -142,21 +151,19 @@ def train_model(optimizer: tf.train.Optimizer, loss: tf.Tensor):
         An Operation that updates the variables in `var_list`
         & also increments `global_step`.
     """
-    # Minimize the loss.
-    # global_step = tf.get_variable("global_step", shape=(),
-    #                               initializer=tf.zeros_initializer,
-    #                               trainable=False)
-    # return optimizer.minimize(loss=loss, global_step=global_step)
     return optimizer.minimize(loss=loss, global_step=tf.train.get_or_create_global_step())
 
 
-if __name__ == '__main__':
+def main():
     # Load training and testing dataset.
     train_data, test_data = load_data()
 
     # Dataset dimensions.
     n_train, n_test = train_data[0].shape[0], test_data[1].shape[0]
     n_features, n_labels = test_data[0].shape[1], test_data[1].shape[1]
+
+    print(('\nNumber of training samples: {:,}\nNumber of testing samples: {:,}'
+           '\nFeatures: {:,}\tLabels: {:,}\n').format(n_train, n_test, n_features, n_labels))
 
     # Features and labels placeholder.
     X_plhd = tf.placeholder(dtype=tf.float32, shape=[None, 4])
@@ -171,17 +178,23 @@ if __name__ == '__main__':
     iterator = dataset.make_initializable_iterator()
     features, labels = iterator.get_next()
 
+    # Initialize model & optimizer.
     model = Network()
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
+    # Calculate the loss & train the model.
     loss = loss_func(model=model, features=features, labels=labels)
     train_op = train_model(optimizer=optimizer, loss=loss)
+
+    # Retrieve the optimizer's global step.
     global_step = tf.train.get_global_step()
 
+    # TODO(victor-iyiola): Estimate model's accuracy on test set.
     logits = model(features)
     correct = tf.equal(tf.argmax(logits, axis=1), tf.argmax(labels, axis=1))
     accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
+    # Running the computational graph (on tf.get_default_graph).
     with tf.Session() as sess:
         # Initialize global variables.
         sess.run(tf.global_variables_initializer())
@@ -194,40 +207,12 @@ if __name__ == '__main__':
             for batch in range(n_train // batch_size):
                 # Train the network.
                 _, _loss, _global_step, _acc = sess.run([train_op, loss, global_step, accuracy])
+
+                # Log training progress.
                 print(('\rEpoch {:,} of {:,}\tGlobal step: {:,}'
-                       '\tLoss: {:.3f}\tAcc: {:.2%}').format(epoch + 1, epochs, _global_step,
-                                                             _loss, _acc), end='')
+                       '\tLoss: {:.3f}\tAcc: {:.2%}')
+                      .format(epoch + 1, epochs, _global_step, _loss, _acc), end='')
 
-"""
-    # Get train data.
-    train_iter = train.make_one_shot_iterator()
-    X_train, y_train = train_iter.get_next()
 
-    # Get test data.
-    test_iter = test.make_one_shot_iterator()
-    X_test, y_test = test_iter.get_next()
-
-    # Initialize model & optimizer.
-    model = Network()
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-
-    # Calculate the loss & train the model.
-    loss = loss_func(model=model, features=X_train, labels=y_train)
-    train_op = train_model(optimizer=optimizer, loss=loss)
-
-    # Retrieve the optimizer's global step.
-    global_step = tf.train.get_global_step()
-
-    # Run within session.
-    with tf.Session() as sess:
-        # Initialize global variables.
-        sess.run(tf.global_variables_initializer())
-        # Go through training epochs.
-        for epoch in range(epochs):
-
-            for batch in range(120 // batch_size):
-                # Train the model.
-                _, _loss, _global_step = sess.run([train_op, loss, global_step])
-                print(('\rEpoch {:,} of {:,}\tGlobal step {:,}'
-                       '\tLoss {:.3f}').format(epoch + 1, epochs, _global_step, _loss), end='')
-"""
+if __name__ == '__main__':
+    main()
